6704971bcb0e640ba8441ce2a0706cc73c8ebfd6
Aleena Thomas <alithomas279@gmail.com>
2025-04-06 15:00:10 +0300

    Added Genre in admin

---
+++ BookShoppingCartMvcUI/Repositories/IGenreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShoppingCartMvcUI.Repositories
+{
+    public interface IGenreRepository
+    {
+        Task AddGenre(Genre genre);
+        Task UpdateGenre(Genre genre);
+        Task DeleteGenre(Genre genre);
+        Task<Genre?> GetGenreById(int id);
+        Task<IEnumerable<Genre>> GetGenres();
+    }
+}
