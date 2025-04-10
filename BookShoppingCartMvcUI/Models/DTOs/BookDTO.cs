bd00db17c99750ebfb3359458b3051e625b27fc2
Aleena Thomas <alithomas279@gmail.com>
2025-04-10 14:10:31 +0300

    added book view in admin

---
+++ BookShoppingCartMvcUI/Models/DTOs/BookDTO.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCartMvcUI.Models.DTOs
+{
+    public class BookDTO
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public String? BookName { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public String? AuthorName { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string? Image { get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public IFormFile? ImageFile { get; set; }
+        public IEnumerable<SelectListItem>? GenreList { get; set; }
+
+    }
+
+}
---
+++ BookShoppingCartMvcUI/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BookShoppingCartMvcUI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task AddBook(Book book);
+        Task DeleteBook(Book book);
+        Task<IEnumerable<Book>> GetBooks();
+        Task<Book?> GetBookById(int id);
+        Task UpdateBook(Book book);
+
+
+    }
+}
